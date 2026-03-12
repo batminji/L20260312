50bda794c54c60942d4832d32a8b820d7b239aec
BATMINJI <minjiconan@naver.com>
2026-03-12 09:13:52 +0900

    배열 전달할 때 사이즈도 전달

--- main.cpp
+++ main.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 
-void Init(int* Numbers);
-void Shuffle(int* Numbers);
-void PickNumbers(int* Numbers, int* MyNumbers);
-void PrintMyNumbers(int* MyNumbers);
+#define NUMBER_SIZE 45
+#define PICK_NUMBER_SIZE 6
+
+void Init(int* Numbers, int Size);
+void Shuffle(int* Numbers, int Size);
+void PickNumbers(int* Numbers, int* MyNumbers, int Size);
+void PrintMyNumbers(int* MyNumbers, int Size);
 
 int main()
 {
-	int Numbers[45] = { 0, };
+	int Numbers[NUMBER_SIZE] = { 0, };
 	int MyNumbers[6] = { 0, };
 
-	Init(Numbers);
-	Shuffle(Numbers);
-	PickNumbers(Numbers, MyNumbers);
-	PrintMyNumbers(MyNumbers);
+	Init(Numbers, NUMBER_SIZE);
+	Shuffle(Numbers, NUMBER_SIZE);
+	PickNumbers(Numbers, MyNumbers, PICK_NUMBER_SIZE);
+	PrintMyNumbers(MyNumbers, PICK_NUMBER_SIZE);
 
 	return 0;
 }
 
-void Init(int* Numbers)
+void Init(int* Numbers, int Size)
 {
-	for (int i = 0; i < 45; ++i)
+	for (int i = 0; i < Size; ++i)
 	{
 		Numbers[i] = i + 1;
 	}
 }
 
-void Shuffle(int* Numbers)
+void Shuffle(int* Numbers, int Size)
 {
 	srand((unsigned int)time(nullptr));
 
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i < Size * 10; ++i)
 	{
 		int FirstIndex = rand() % 45;
 		int SecondIndex = rand() % 45;
@@ -41,18 +44,18 @@ void Shuffle(int* Numbers)
 	}
 }
 
-void PickNumbers(int* Numbers, int* MyNumbers)
+void PickNumbers(int* Numbers, int* MyNumbers, int Size)
 {
-	for (int i = 0; i < 6; ++i)
+	for (int i = 0; i < Size; ++i)
 	{
 		MyNumbers[i] = Numbers[i];
 	}
 }
 
-void PrintMyNumbers(int* MyNumbers)
+void PrintMyNumbers(int* MyNumbers, int Size)
 {
 	std::cout << "·£´ý ¼ýÀÚ 6°³ : ";
-	for (int i = 0; i < 6; ++i)
+	for (int i = 0; i < Size; ++i)
 	{
 		std::cout << MyNumbers[i] << " ";
 	}
